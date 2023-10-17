b0bd5f83857d91104dc1547e70a6f093663a8054
jb318 <johnblute66@gmail.com>
2023-10-17 13:35:57 -0400

    modified

--- practice.js
+++
@@ -1,7 +0,0 @@
-let languages = [
-    "HTML", "CSS", "JavaScript"];
-  
-  for (let i=0; i < languages.length; i++) {
-  let language = languages[i];
-  console.log("I am excited to learn " + language);
-  }
